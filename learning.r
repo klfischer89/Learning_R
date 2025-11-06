adbcb535b196f46081aede921bce21cc38584399
klfischer89 <fischer-k4@web.de>
2025-11-06 08:19:11 +0100

    Adding Boxplot of Iris

--- learning.r
+++ learning.r
@@ -237,4 +237,6 @@ summary(iris)
 str(iris)
 # Basic Plotting
 plot(iris$Sepal.Length, iris$Sepal.Width, col=iris$Species)
-
+# Boxplot
+boxplot(Sepal.Length ~ Species, data=iris)
+boxplot(iris[1:4])
